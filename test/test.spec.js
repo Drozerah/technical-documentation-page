a4e670fff47cd06298e24e48a415f9983829cbd8
Drozerah <drozerah@gmail.com>
2019-09-30 18:25:27 +0200

    test: implement tests #1 & #2 according to specifications

--- test/test.spec.js
+++ test/test.spec.js
@@ -6,7 +6,7 @@
   if (window.location.search === '?test=1') {
     require('../node_modules/mocha/mocha.css')
     const assert = require('chai').assert
-    const expect = require('chai').expect
+    // const expect = require('chai').expect
     const { mocha } = require('mocha')
     mocha.setup('bdd') // minimal setup
 
@@ -20,11 +20,41 @@
     div.style.background = 'white'
     document.body.appendChild(div)
 
-    console.warn('Mocha is running ./test/test.spec.js')
-
-    describe('Page:', () => {
-      it('should have 1 element with an id of "mocha"', () => {
+    // Mocha
+    describe('Mocha:', function () {
+      it('page should have an element with an id of "mocha"', function () {
         assert.isNotNull(document.getElementById('mocha'), 'There is no element with an id of "mocha"')
+        console.warn('Mocha is running ./test/test.spec.js')
+      })
+    })
+
+    // Testing the page
+    describe('Page:', function () {
+      it('should have a <main> element with a corresponding id="main-doc"', function () {
+        assert.isNotNull(document.getElementById('main-doc'), 'There is no element with an id of "main-doc"')
+        assert.strictEqual(document.getElementById('main-doc').nodeName, 'MAIN', 'The "main-doc" element should be a <main>')
+      })
+      describe('<main> element:', function () {
+        it('should contain several <section> elements, each with a class of "main-section". There sould be a minimum of 5', function () {
+          // get all ".main-section" elements within the "#main-doc" element
+          const sections = document.querySelectorAll('#main-doc .main-section')
+
+          // get all ".main-section" elements as NodeList
+          const t = document.querySelectorAll('.main-section')
+
+          // check if all ".main-section" elements are <section> elements
+          // filtering the NodeList elements by nodeName returns an array
+          // the length or that array corresponds to the number of elements with the class
+          // ".main-section" that are <section> elements
+          const r = Array.from(t).filter(elm => {
+            return elm.nodeName === 'SECTION'
+          })
+
+          assert.isAbove(sections.length, 0, 'There are no .main-section elements within #main-doc')
+          assert.isAtLeast(sections.length, 5, 'There are not at least 5 elements with the class of "main-section"')
+          assert.strictEqual(document.querySelectorAll('.main-section').length, sections.length, 'All of the page\'s .main-section elements should be within #main-doc')
+          assert.strictEqual(r.length, t.length, 'Not all of the elements with the class of "main-section" are <section> elements')
+        })
       })
     })
     mocha.run()
