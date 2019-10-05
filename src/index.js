42a4dd59ae445ca742383eaa4a324d4cd77ece43
Drozerah <drozerah@gmail.com>
2019-10-05 15:09:01 +0200

    feat: add ie forEach method polyfill

--- src/index.js
+++ src/index.js
@@ -19,6 +19,20 @@ import hljs from 'highlight.js'
 // run()
 
 document.addEventListener('DOMContentLoaded', (event) => {
+  /**
+  * forEach method polyfill for IE11
+  * @{link} https://gist.github.com/bob-lee/e7520bfcdac266e5490f40c2759cc955
+  */
+  if ('NodeList' in window && !NodeList.prototype.forEach) {
+    // console.info('polyfill for IE11')
+    NodeList.prototype.forEach = function (callback, thisArg) {
+      thisArg = thisArg || window
+      for (var i = 0; i < this.length; i++) {
+        callback.call(thisArg, this[i], i, this)
+      }
+    }
+  }
+
   // init highlight.js
   document.querySelectorAll('code').forEach(block => hljs.highlightBlock(block))
 
